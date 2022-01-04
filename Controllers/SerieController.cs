cd33de4993785ae97e9dc4ac38f2b1cd00be7250
Gabriel <gabrieleliasmega@gmail.com>
2022-01-04 00:00:15 -0300

    api rest para o projeto DioSeries finalizada

---
+++ Controllers/SerieController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DIO.Series;
+using DIO.Series.Interfaces;
+
+namespace DIOSeriesWeb.Controllers
+{
+    [Route("[controller]")]
+
+    public class SerieController : Controller
+    {
+        /*
+        GET = Retornar algo
+        POST = Inserir algo
+        PUT = Alterar algo
+        DELETE = Excluir algo
+        */
+
+        private readonly IRepositorio<Serie> _repositorioSerie;
+
+        public SerieController(IRepositorio<Serie> repositorioSerie)
+        {
+            _repositorioSerie = repositorioSerie;
+        }
+
+        [HttpGet("")]
+        public IActionResult Lista()
+        {
+            return Ok(_repositorioSerie.Lista().Select(s => new SerieModel(s)));
+
+            IList<SerieModel> series = new List<SerieModel>();
+            
+            return Ok(series);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualiza(int id, [FromBody] SerieModel model)
+        {
+            _repositorioSerie.Atualiza(id, model.ToSerie());
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Exclui(int id)
+        {
+            _repositorioSerie.Exclui(id);
+            return NoContent();
+        }
+
+        [HttpPost("")]
+        public IActionResult Insere([FromBody] SerieModel model)
+        {
+            model.Id = _repositorioSerie.ProximoId();
+
+            Serie serie = model.ToSerie();
+                        
+            _repositorioSerie.Insere(model.ToSerie());
+            return Created("", serie);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Consulta(int id)
+        {
+            return Ok(new SerieModel(_repositorioSerie.Lista().FirstOrDefault(s => s.Id == id)));
+        }
+    }
+}
